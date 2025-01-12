43293562610e2536575dc04a2bceb57c8aea0e74
gagantripathi22 <gagantripathi22@gmail.com>
2025-01-13 00:26:40 +0530

    add: api status check endpoint

--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -1,42 +1,46 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const postController = require('../controller/post.controller');
-const basicAuthentication = require('../middleware/basicAuth');
-const verifyToken = require('../middleware/verifyToken');
+const postController = require("../controller/post.controller");
+const basicAuthentication = require("../middleware/basicAuth");
+const verifyToken = require("../middleware/verifyToken");
 
 router.post(
-  '/create',
+  "/create",
   basicAuthentication,
   verifyToken.verifyToken,
-  postController.createPost
+  postController.createPost,
 );
-router.get('/get/posts/:page', basicAuthentication, postController.getPosts);
+router.get("/get/posts/:page", basicAuthentication, postController.getPosts);
 router.get(
-  '/get/post/:url',
+  "/get/post/:url",
   basicAuthentication,
-  postController.getPostUsingUrl
+  postController.getPostUsingUrl,
 );
 router.delete(
-  '/delete/post/:id',
+  "/delete/post/:id",
   basicAuthentication,
   verifyToken.verifyToken,
-  postController.deletePost
+  postController.deletePost,
 );
 router.patch(
-  '/update/post/:id',
+  "/update/post/:id",
   basicAuthentication,
   verifyToken.verifyToken,
-  postController.updatePost
+  postController.updatePost,
 );
 router.post(
-  '/create/post',
+  "/create/post",
   basicAuthentication,
   verifyToken.verifyToken,
-  postController.createNewPost
+  postController.createNewPost,
 );
 
-router.get('/search', basicAuthentication, postController.search);
+router.get("/search", basicAuthentication, postController.search);
 
-router.get('/searchnew', basicAuthentication, postController.searchNew);
+router.get("/searchnew", basicAuthentication, postController.searchNew);
+
+router.get("/status", (req, res) => {
+  res.send("OK");
+});
 
 module.exports = router;
